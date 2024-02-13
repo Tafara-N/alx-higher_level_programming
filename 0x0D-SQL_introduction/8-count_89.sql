a986da783b2ad5080c24371e7ccede4e2a706f8a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-13 15:52:20 +0200

    A script that displays the number of records with id = 89 in the table first_table of the database.

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- A script that displays the number of records with id = 89 in the table first_table of the database
-SELECT COUNT(id) FROM first_table WHERE id=89;
+SELECT COUNT(id) FROM first_table WHERE id = 89;
