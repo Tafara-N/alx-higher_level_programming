6a13898ddb267d91ea556f760a2d7c5d001b0493
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-13 15:43:55 +0200

    A script that lists all records of the table second_table of the database hbtn_0c_0.

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- A script that lists all records of the table second_table of the database
+--  Results display both the score and the name (in that order)
+--  Records are ordered by score (top first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC
