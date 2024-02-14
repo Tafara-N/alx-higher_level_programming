54e2f06d6f1d6587b91e2d97eef035f6870dfcf7
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-14 15:48:28 +0200

    A script that creates the MySQL server user user_0d_1 with all the privileges.

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,10 @@
+-- A script that creates the MySQL server user user_0d_1
+--  has all privileges on the said MySQL server
+--  password is set to user_0d_1_pwd
+
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost';
