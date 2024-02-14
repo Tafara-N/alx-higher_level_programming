f2a6292c3de9a201d40d86ac84a54af182f8222a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-14 16:13:27 +0200

    A script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa).

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -11,4 +11,4 @@ CREATE TABLE IF NOT EXISTS cities (
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id);
