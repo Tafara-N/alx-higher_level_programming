f67a9dfff3218871befa320367ad12457da15090
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-14 16:49:00 +0200

    A script that lists all shows contained in the database hbtn_0d_tvshows.

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,13 @@
+-- Importing the database dump from hbtn_0d_tvshows
+-- A script that lists all shows contained in the database hbtn_0d_tvshows
+--  each record should display: tv_shows.title - tv_show_genres.genre_id
+--  results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- You can use only one SELECT statement
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
