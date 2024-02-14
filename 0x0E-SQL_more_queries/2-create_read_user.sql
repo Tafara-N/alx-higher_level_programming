861b82be9cf1dedbc7bc52194da28390430dd393
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-14 16:02:57 +0200

    A script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,8 +3,5 @@
 --  user_0d_2 password is set to user_0d_2_pwd
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'User_0d_2_pwd@@';
-GRANT SELECT
-    ON hbtn_0d_2.* TO user_0d_2@localhost;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'User_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
