0f68788fe16b27335faca9c109511018e58aeac1
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-16 16:10:52 +0200

    A script on how to import data from a sql file into your database_name.

---
+++ 0x0D-SQL_introduction/104-import_file.sql
@@ -0,0 +1,7 @@
+-- A script on how to import data from sql file into your database
+-- Inside your mySQL terminal
+
+USE database_name;
+SOURCE /path/to/file.sql
+SHOW TABLES;
+SELECT * FROM table_name;
