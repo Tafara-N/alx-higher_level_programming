1ec2de5b148073cf2a62af98b88f94e7bbf0d712
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-02-16 16:47:42 +0200

    A script that creates the MySQL server user user_0d_1 with all the privileges.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,9 +2,7 @@
 --  has all privileges on the said MySQL server
 --  password is set to user_0d_1_pwd
 
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
