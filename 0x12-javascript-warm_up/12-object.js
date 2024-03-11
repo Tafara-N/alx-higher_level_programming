7b6886c71c8a7051f62e9b08f6b9717eef6dd02d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-11 11:52:41 +0200

    A script that replaces the value 12 with 89.

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  // My code
-  myObject.value = 89;
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// My code
+myObject.value = 89;
+console.log(myObject);
