a3bdbdd4d3e6cf42e8e3b335a4315c23fffbed7a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-11 11:18:49 +0200

    A script that prints 3 lines.

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\n');
+console.log('Python is cool\n');
+console.log('Javascript is amazing');
