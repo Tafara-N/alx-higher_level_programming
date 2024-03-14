c4de173db11d3edbef38604bd9a3a09658cd574c
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-15 01:23:46 +0200

    A script that lists all states values from the database 'hbtn_0e_0_usa' that matches a name argument.

---
+++ 0x0F-python-object_relational_mapping/2-filter_my_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+Script takes arguments and displays all values in the `states` table
+from the database 'hbtn_0e_0_usa' where name matches the argument
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    Connects to the 'hbtn_0e_0_usa' database and execute a query that
+    fetches all data from the `states` table where name matches the argument
+    """
+
+    if len(argv) != 5:
+        print(
+            "Usage: ./script.py <username> <password>\
+            <database> <name_to_search>"
+        )
+        exit(1)
+
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]
+    )
+
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}'\
+        ORDER BY id ASC".format(argv[4])
+
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
