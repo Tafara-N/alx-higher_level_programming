a753b5f3c4d11042a794152860467e0808bd8042
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-16 06:51:42 +0200

    A script that lists all states values from the database 'hbtn_0e_0_usa' that matches a name argument.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,7 +2,7 @@
 
 """
 Script takes arguments and displays all values in the `states` table
-from the database 'hbtn_0e_0_usa' where name matches the argument
+from the database 'hbtn_0e_0_usa' where `name` matches the argument
 """
 
 import MySQLdb
@@ -12,28 +12,36 @@ from sys import argv
 if __name__ == '__main__':
     """
     Connects to the 'hbtn_0e_0_usa' database and executes a query that
-    fetches all data from the `states` table where name matches the argument
+    fetches all data from the `states` table where `name` matches the argument
     """
 
     if len(argv) != 5:
         print("Usage: ./script.py <username> <password>\
-            <database> <name_to_search>")
+            <database> <name_to_search>"
+        )
         exit(1)
 
     db = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
 
-    cur = db.cursor()
+    cursor = db.cursor()
 
     # Query with string formatting to prevent an SQL injection
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE\
-                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
+    query = ("SELECT * FROM `states` WHERE `name` LIKE\
+        BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]}
+    )
+
+    cursor.execute(query)
 
     rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+    for record in records:
+        print(record)
 
-    cur.close()
+    cursor.close()
     db.close()
