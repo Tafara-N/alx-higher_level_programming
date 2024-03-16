2ce075f7f34a93176966515b42bb3855ac16e528
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-16 07:13:41 +0200

    A script that lists all cities from the database 'hbtn_0e_4_usa'.

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -26,8 +26,7 @@ if __name__ == '__main__':
     cur.execute("SELECT cities.id, cities.name, states.name\
         FROM `cities`\
         JOIN `states` ON cities.state_id = states.id\
-        ORDER BY `id` ASC"
-    )
+        ORDER BY `id` ASC")
 
     records = cursor.fetchall()
 
