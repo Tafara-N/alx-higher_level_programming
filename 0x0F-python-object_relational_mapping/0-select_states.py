982300796df1c0ab1e0ba7182bae03dfa37d1278
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-16 06:29:11 +0200

    A script that lists all states from the database 'hbtn_0e_0_usa'.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -22,11 +22,11 @@ if __name__ == '__main__':
         port=3306
         )
 
-    cur = db.cursor()
+    cursor = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    records = cur.fetchall()
+    records = cursor.fetchall()
 
     for record in records:
         print(record)
