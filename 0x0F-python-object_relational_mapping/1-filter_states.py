fe1b1bb920069587163f7d75cdb44321db7035e2
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-16 06:48:57 +0200

    A script that lists all states from the database 'hbtn_0e_0_usa'.

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+A script that lists all states from the database 'hbtn_0e_0_usa'.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """
+    Connects to the 'hbtn_0e_0_usa' database and executes a query
+    that fetches all data from the `states` table
+    """
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM `states`\
+        WHERE `name` LIKE BINARY 'N%'\
+        ORDER BY `id` ASC"
+
+    cursor.execute(query)
+
+    records = cursor.fetchall()
+
+    for record in records:
+        print(record)
