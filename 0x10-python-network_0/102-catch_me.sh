66aa34d08b7de08493dd41a2027c330d2b5fd97b
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-27 06:48:58 +0200

    Script makes a request that makes the server respond with 'You got me!'.

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
 
-
 curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
