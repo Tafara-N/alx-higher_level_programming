9422d3219f296d90362e5894842304989b3fdc6d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-28 09:58:19 +0200

    Script takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Script that takes in a URL, and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    req = requests.post(url, data=payload)
+    print(req.text)
