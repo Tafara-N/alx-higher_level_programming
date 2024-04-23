b8b6537ea4c9d1bc5d899dfcb174ebab1bcc8b99
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-23 18:44:57 +0200

    Script that gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,10 +2,10 @@
 // Script that gets the contents of a webpage and stores it in a file
 
 const request = require('request');
-const fileSystem = require('fileSystem');
+const fileSys = require('fs');
 
 request(process.argv[2], function (err, response, body) {
   if (err == null) {
-    fileSystem.writeFileSync(process.argv[3], body);
+    fileSys.writeFileSync(process.argv[3], body);
   }
 });
