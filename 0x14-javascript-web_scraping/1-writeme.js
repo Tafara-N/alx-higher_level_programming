0c562cdef1955b020b374e641759496d273e1fe1
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-23 18:06:25 +0200

    A script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+// Script writes a string to a file
 
-const fs = require('fs');
+const filesystem = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
+filesystem.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
