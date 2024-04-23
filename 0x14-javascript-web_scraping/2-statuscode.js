f2205b763301d95aa577457dc318e44450eaa4c2
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-23 18:10:25 +0200

    Script display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+// Script display the status code of a GET request
+
+const request = require('request');
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
