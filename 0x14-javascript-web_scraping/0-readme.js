5de0427f69583b763a56c98bfcd3a3980d764c9a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-23 18:04:06 +0200

    Script reads and prints the contents of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Script reads and prints the contents of a file
 
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8', function (err, data) {
+const filesystem = require('fs');
+filesystem.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
     return;
