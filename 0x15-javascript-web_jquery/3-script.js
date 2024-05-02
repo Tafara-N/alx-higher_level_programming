272eb0cf7a75506cf05cf45832d331133d948f01
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-02 15:31:59 +0200

    Script adds the class red to the <header> element when the user clicks on the tag DIV#red_header.

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+// Script adds the class red to the <header> element
+// when the user clicks on the tag DIV#red_header
+
+$('div#red_header').click(function () {
+  $('header').addClass('red');
+});
