edc37f9b59c7f7c01586153eadd25eebd6ce9950
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-02 16:10:42 +0200

    Script fetches and displays the value of hello from that fetch in the HTML tag DIV#hello.

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,2 +1,12 @@
 // Script fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and
 // displays the value of hello from that fetch in the HTML tag DIV#hello
+
+$(document).ready(function () {
+	$.ajax({
+		type: 'GET',
+		url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+		success: function (data) {
+			$('DIV#hello').append(data.hello);
+		},
+	});
+});
