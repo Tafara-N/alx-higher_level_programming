65a378e79107e22517240293d84c58337ecd5375
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-02 15:42:16 +0200

    Script adds a <li> element to a list when the user clicks on the tag DIV#add_item.

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,2 +1,6 @@
 // Script adds a <li> element to a list when the user clicks on the tag DIV#add_item
 
+$('div#add_item').click(function () {
+  const element = '<li>Item</li>';
+  $('ul.my_list').append(element);
+});
