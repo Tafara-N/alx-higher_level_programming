d8c26aa985cc96776151860c5886e89c801414f3
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-02 16:00:40 +0200

    Script fetches the character name from a URL.

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,12 @@
+// Script fetches the character name from this URL:
+// https://swapi-api.alx-tools.com/api/people/5/?format=json
+
+$(document).ready(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi.co/api/people/5/?format=json',
+    success: function (data) {
+      $('DIV#character').text(data.name);
+    }
+  });
+});
