73d1ec5f4b3450985f3c6f775a2a865e9b2c6aae
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-02 16:39:14 +0200

    Script updates the text color of the <header> element to red (#FF0000).

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Script updates the text color of the <header> element to red (#FF0000)
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
