aa3b3d0726582cd3e8feee3c2d14ecc3a2d5af62
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-02 16:42:12 +0200

    Script adds, removes and clears LI elements from a list when the user clicks.

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Script adds, removes and clears LI elements from a list when the user clicks
+
+$('document').ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last').remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
