541cbb72bc00a3ee355ec3532c43182fb0a26055
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-02 16:03:07 +0200

    Script fetches and lists the title for all movies by using a URL.

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,14 @@
+// Script fetches and lists the title for all movies by using this URL:
+// https://swapi-api.alx-tools.com/api/films/?format=json
+
+$(document).ready(function () {
+	$.ajax({
+		type: 'GET',
+		url: 'https://swapi.co/api/films/?format=json',
+		success: function (data) {
+			$.each(data.results, function (i, film) {
+				$('UL#list_movies').append('<li>' + film.title + '</li>');
+			});
+		},
+	});
+});
