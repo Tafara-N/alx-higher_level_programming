641ca890e6ec196a5eb2d3cac6654e3d6b87c4f7
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-02 15:34:40 +0200

    Script toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header.

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+// Script toggles the class of the <header> element
+// when the user clicks on the tag DIV#toggle_header
+
+$('div#toggle_header').click(function () {
+  $('header').ToggleClass('red');
+});
