f4eecdf7edd335324d0d71be4ba8b6e7cb0f3916
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-02 15:58:12 +0200

    Script updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header.

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,8 @@
+// Script updates the text of the <header> element to
+// New Header!!! when the user clicks on DIV#update_header
+
+$(document).ready(function () {
+  $('DIV#update_header').click(function () {
+    $('HEADER').text('New Header!!!');
+  });
+});
