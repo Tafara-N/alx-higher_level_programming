37574982d60dcacedf13d882db6d499a269c1ef2
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-01 22:17:03 +0200

    Script creates the database `hbtn_0d_2` and the user `user_0d_2`

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,9 @@
 --  user_0d_2 has only SELECT privilege in database hbtn_0d_2
 --  user_0d_2 password is set to user_0d_2_pwd
 
+SET GLOBAL validate_password.policy=LOW;
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+SET GLOBAL validate_password.policy=MEDIUM;
+FLUSH PRIVILEGES;
