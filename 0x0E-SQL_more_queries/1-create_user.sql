fbbb19d6682e71a71f5126bb947543b9d505db14
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-01 22:13:43 +0200

    Script creates the MySQL server `user user_0d_1`.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,11 @@
 --  has all privileges on the said MySQL server
 --  password is set to user_0d_1_pwd
 
+SET GLOBAL validate_password.policy=LOW;
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.*
 TO 'user_0d_1'@'localhost';
+SET GLOBAL validate_password.policy=MEDIUM;
+FLUSH PRIVILEGES;
+
